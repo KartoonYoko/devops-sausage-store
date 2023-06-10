b3d0f2ee966d0b2e5171483cdfffaa656ea8df8a
std-016-032 std-016-032 <std-016-032@praktikum-services.ru>
2023-06-10 09:29:54 +0000

    Flyway

---
+++ backend/migrations/R__create_indexes.sql
@@ -0,0 +1,3 @@
+CREATE INDEX idx_order_product_product_id_order_id ON public.order_product USING btree (product_id, order_id);
+CREATE INDEX idx_order_id ON public.orders USING btree (id);
+CREATE INDEX idx_product_id ON public.product USING btree (id);
